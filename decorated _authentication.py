dbd42889ab1dded12410acdb8e5f2af06e0fc503
Ivan <alexeevivan92@gmail.com>
2021-01-03 13:14:50 +0300

    Update decorated _authentication.py

--- decorated _authentication.py
+++ decorated _authentication.py
@@ -23,16 +23,21 @@ menu_list_introductory = """
 # информация, доступная после успешного входа
 menu_list_main = """
 \n1. A list related to the employees of the company «L.V.19 LLC»
-— — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — 
 \n\t1.1 «L.V.19 LLC» list of employees by the position
 \n\t1.2 «L.V.19 LLC» employees salary list by the position
 \n\t1.3 «L.V.19 LLC» employees certification results
 — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — 
 \n2. Documentation related to taxation issues of the company «L.V.19 LLC»
+\n\t2.1 The list of partner companies
+\n\t2.2 Documentation containing information about taxes on premises owned by the company
+\n\t2.3 Contact-list of inspection employees
 — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — 
 \n3. Constituent documents (articles of association, licenses, etc.)
+\n\t3.0.1 Will be updated soon...
 — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — 
 """
+
+# доступные имена пользователей и пароли
 usernames = ["Ivan", "Julia", "Ian"]
 user_1 = usernames[0]
 user_2 = usernames[1]
@@ -48,72 +53,54 @@ expaining_message = ("For further viewing of the information provided below, ple
 print(expaining_message)
 request_message_2 = int(input("\nPlease enter 1 to continue:"))
 while request_message_2!=1:
-    print(Fore.RED, "An invalid response was entered. Try again or return to homepage.", Fore.WHITE)
+    print(Fore.RED)
+    print("An invalid response was entered. Try again or return to homepage.", Fore.WHITE)
     request_message_2 = int(input("\nPlease enter 1 to log in:"))
 if request_message_2==1:
     os.system('cls' if os.name=='nt' else 'clear')
 
 
-# проверка имени пользователя
-def userask(func):
-    def wrapper():
-        print(Fore.GREEN,"Got it.", Fore.WHITE)
-        sleep(2)
+def access_to_login(func):
+    def wrapper(*args):
+        sleep(1)
         os.system('cls' if os.name=='nt' else 'clear')
+        print(Fore.GREEN)
+        func(*args)
+        print(Fore.WHITE)
     return wrapper
-
-
-# проверка пароля
-def pass_ask(func):
-    def wrapper_1():
-        print(Fore.GREEN,"Got it.", Fore.WHITE)
-        sleep(2)
-        os.system('cls' if os.name=='nt' else 'clear')
-    return wrapper_1
 
 
 username_request = str.title(input("Please enter your nickname:"))
 if username_request in usernames:
-    @userask
-    def login(usernames):
-        pass
+    @access_to_login
+    def login():
+        print("Got it.\nUsername accepted:", username_request)
     login()
-else:
-    print(Fore.RED,"\nOops. Nice try, but it is the place with no errors in typing allowed!", Fore.WHITE)
-    StopIteration
-
-
-def pass_ask(func):
-    def wrapper_1():
-        print(Fore.GREEN,"Got it.", Fore.WHITE)
-        sleep(2)
-        os.system('cls' if os.name=='nt' else 'clear')
-    return wrapper_1
-
-
-if username_request in usernames:
-    password_request = int(input("Enter the password:"))
+    password_request = int(input("Enter a password:"))
     if password_request==keys[0] and username_request==user_1:
-        @pass_ask
-        def login_2_step(keys):
-            pass
-        login_2_step()
-        print(Fore.GREEN,"\nNow you are able to discover the files i have.", Fore.CYAN, menu_list_main)
+        @access_to_login
+        def password():
+            print("Got it.\nNow you are able to discover the files i have.")
+            print(Fore.CYAN, menu_list_main)
+        password()
     elif password_request==keys[1] and username_request==user_2:
-        @pass_ask
-        def login_2_step(keys):
-            pass
-        login_2_step()
-        print(Fore.GREEN,"\nNow you are able to discover the files i have.", Fore.CYAN, menu_list_main)
+        @access_to_login
+        def password():
+            print("Got it.\nNow you are able to discover the files i have.")
+            print(Fore.CYAN, menu_list_main)
+        password()
     elif password_request==keys[2] and username_request==user_3:
-        @pass_ask
-        def login_2_step(keys):
-            pass
-        login_2_step()
-        print(Fore.GREEN,"\nNow you are able to discover the files i have.", Fore.CYAN, menu_list_main)
+        @access_to_login
+        def password():
+            print("Got it.\nNow you are able to discover the files i have.")
+            print(Fore.CYAN, menu_list_main)
+        password()
     else:
         os.system('cls' if os.name=='nt' else 'clear')
-        print(Fore.RED, "\nOops. Nice try, but it is the place with no errors in typing allowed!", Fore.WHITE)
+        print(Fore.RED)
+        print("\nOops. Nice try, but it is the place with no errors in typing allowed!", Fore.WHITE)
         StopIteration
 else:
+    print(Fore.RED)
+    print("\nOops. Nice try, but it is the place with no errors in typing allowed!", Fore.WHITE)
     StopIteration
